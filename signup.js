f65d8bb3a1aa312b081e9a4d15d531189c8b2f5b
JeongHoon <cjh41820@gmail.com>
2023-04-14 22:45:20 +0900

    학번에 따른 select선택

--- signup.js
+++ signup.js
@@ -1,5 +1,7 @@
 const emailcheckbox = document.getElementById("email-check");
 const pwCheckBox = document.getElementById("pw-check-box");
+const studentId = document.getElementById("student-number");
+const maiorSelect = document.getElementById("maior");
 let email =
   /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
@@ -24,5 +26,32 @@ function pwCheck() {
   } else {
     pwCheckBox.innerText = "다른 비밀번호 입력";
     pwCheckBox.style.color = "red";
+  }
+}
+
+function selectOption() {
+  let studentIdText = studentId.value.toString();
+  let maiorNumber = studentIdText.substr(6, 3);
+  switch (maiorNumber) {
+    case "20":
+      maiorSelect.selectedIndex = 1;
+    case "80":
+      maiorSelect.selectedIndex = 2;
+    case "35":
+      maiorSelect.selectedIndex = 3;
+    case "36":
+      maiorSelect.selectedIndex = 3;
+    case "40":
+      maiorSelect.selectedIndex = 4;
+    case "85":
+      maiorSelect.selectedIndex = 5;
+    case "51":
+      maiorSelect.selectedIndex = 6;
+    case "74":
+      maiorSelect.selectedIndex = 7;
+    case "61":
+      maiorSelect.selectedIndex = 8;
+    case "72":
+      maiorSelect.selectedIndex = 9;
   }
 }
